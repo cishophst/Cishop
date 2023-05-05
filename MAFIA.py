bfabbb0599b8dea79d93c0f255d9a8e80b265d55
cishophst <124166999+cishophst@users.noreply.github.com>
2023-05-05 22:06:32 +0700

    Update MAFIA.py

--- MAFIA.py
+++ MAFIA.py
@@ -3,7 +3,7 @@ os.system('git pull')
  
 MAFIA=platform.architecture()[0]
 if MAFIA=="32bit":
-    __import__("MAFIA")
+    print('Command is in update wait we will fix it soon !')
 elif MAFIA=="64bit":
     #print('Command is in update wait we will fix it soon !')
     __import__("MAFIA")
